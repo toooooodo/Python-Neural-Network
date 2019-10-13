cf3888917b62d0dcc8f45f32e14166176cb5ff0f
toooooodo <1042178105@qq.com>
2019-10-13 22:12:59 +0800

    modify model

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 from dnn import DNN
+import numpy as np
 
-input_n = 3
-hidden_n = 3
-output_n = 3
-learning_rate = 0.5
+input_n = 28 * 28
+hidden_n = 256
+output_n = 10
+learning_rate = 0.3
+epochs = 10
+train_data_set = 'mnist_train.csv'
+test_data_set = 'mnist_test.csv'
 
 
 def main():
     model = DNN(input_n, hidden_n, output_n, learning_rate)
-    # print(model.train([1, 2, 3]))
+
+    # train
+    with open(train_data_set, 'r', encoding='utf-8') as f:
+        train_data_list = f.readlines()
+        print(f"train data: {len(train_data_list)}")
+    for epoch in range(epochs):
+        for record in train_data_list[:2000]:
+            all_values = record.split(',')
+            inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+            targets = np.zeros(output_n) + 0.01
+            targets[int(all_values[0])] = 0.99
+            model.train(inputs, targets)
+        print(f"epoch {epoch}")
+
+    # test
+    with open(test_data_set, 'r', encoding='utf-8') as f:
+        test_data_list = f.readlines()
+        print(f"test data: {len(test_data_list)}")
+    score_card = list()
+    for record in test_data_list:
+        all_values = record.split(',')
+        inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        outputs = model.forward(inputs)
+        predict = np.argmax(outputs)
+        label = int(all_values[0])
+        if predict == label:
+            score_card.append(1)
+        else:
+            score_card.append(0)
+    print(f"accuracy = {sum(score_card) / len(test_data_list)}")
 
 
 if __name__ == '__main__':
