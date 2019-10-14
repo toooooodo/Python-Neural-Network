57b112843ddb391924063b17fe39054284f2d3b4
toooooodo <1042178105@qq.com>
2019-10-14 21:56:22 +0800

    add bias

--- dnn.py
+++ dnn.py
@@ -12,10 +12,12 @@ class DNN:
         """
         self.layers = layers
         self.lr = learning_rate
-        # network weights
+        # network weights, biases
         self.weights = []
+        self.biases = []
         for i in range(len(self.layers) - 1):
             self.weights.append(np.random.rand(self.layers[i + 1], self.layers[i]) - 0.5)
+            self.biases.append(np.random.rand(self.layers[i + 1], 1) - 0.5)
         self.activation_f = lambda x: special.expit(x)
 
     def train(self, inputs_list, target_list):
@@ -23,26 +25,32 @@ class DNN:
         train the neural network
         :param inputs_list: inputs of neural network
         :param target_list:
-        :return:
+        :return: loss
         """
         inputs = np.array(inputs_list, ndmin=2).T
         targets = np.array(target_list, ndmin=2).T
 
         outputs = []  # outputs of each layer
         x = inputs
-        for w in self.weights:
-            layer_inputs = np.dot(w, x)
+        for w, b in zip(self.weights, self.biases):
+            layer_inputs = np.dot(w, x) + b
             layer_outputs = self.activation_f(layer_inputs)
             outputs.append(x)
             x = layer_outputs
         outputs.append(x)
 
         errors = targets - outputs[-1]  # final errors
+        loss = np.power(errors, 2).sum()
         for i in range(len(outputs) - 1, 0, -1):
             # update the weights
-            # delta_W_jk = alpha * Ek * Ok * (1 - Ok) * Oj.T
-            self.weights[i - 1] += self.lr * np.dot((errors * outputs[i] * (1 - outputs[i])), outputs[i - 1].T)
+            # delta_W_jk = alpha * Ek * Ok * (1 - Ok) @ Oj.T
+            # delta_B_k = alpha * Ek * Ok * (1 - Ok)
+            b_delta = errors * outputs[i] * (1 - outputs[i])
+            self.biases[i - 1] += self.lr * b_delta
+            self.weights[i - 1] += self.lr * np.dot(b_delta, outputs[i - 1].T)
             errors = np.dot(self.weights[i - 1].T, errors)  # back propagating errors
+
+        return loss
 
     def forward(self, inputs_list):
         """
@@ -51,7 +59,7 @@ class DNN:
         :return: outputs of neural network
         """
         x = np.array(inputs_list, ndmin=2).T  # convert inputs to 2D
-        for w in self.weights:
-            x = np.dot(w, x)
+        for w, b in zip(self.weights, self.biases):
+            x = np.dot(w, x) + b
             x = self.activation_f(x)
         return x
--- main.py
+++ main.py
@@ -1,45 +1,50 @@
 from dnn import DNN
 import numpy as np
+import random
 
-layers = [28 * 28, 200, 10]
+layers = [28 * 28, 256, 10]
 learning_rate = 0.1
 epochs = 5
 train_data_set = 'mnist_train.csv'
 test_data_set = 'mnist_test.csv'
 
+model = DNN(layers, learning_rate)
+np.random.seed(12345)
+random.seed(12345)
+
 
 def main():
-    model = DNN(layers, learning_rate)
-
-    # train
     with open(train_data_set, 'r', encoding='utf-8') as f:
         train_data_list = f.readlines()
         print(f"train data: {len(train_data_list)}")
-    for epoch in range(epochs):
+        train_data = []
         for record in train_data_list:
             all_values = record.split(',')
             inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
             targets = np.zeros(layers[-1]) + 0.01
             targets[int(all_values[0])] = 0.99
-            model.train(inputs, targets)
-        print(f"epoch {epoch}")
-
-    # test
+            train_data.append((inputs, targets))
     with open(test_data_set, 'r', encoding='utf-8') as f:
         test_data_list = f.readlines()
         print(f"test data: {len(test_data_list)}")
-    score_card = list()
-    for record in test_data_list:
-        all_values = record.split(',')
-        inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-        outputs = model.forward(inputs)
-        predict = np.argmax(outputs)
-        label = int(all_values[0])
-        if predict == label:
-            score_card.append(1)
-        else:
-            score_card.append(0)
-    print(f"test accuracy = {sum(score_card) / len(test_data_list)}")
+        test_data = []
+        for record in test_data_list:
+            all_values = record.split(',')
+            inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+            label = int(all_values[0])
+            test_data.append((inputs, label))
+    # train
+    for epoch in range(epochs):
+        random.shuffle(train_data)
+        loss = sum(model.train(inputs, targets) for inputs, targets in train_data)
+        print(f"epoch {epoch}, loss = {loss / len(train_data_list)}")
+        # test
+        correct_num = 0
+        for inputs, label in test_data:
+            outputs = model.forward(inputs)
+            predict = np.argmax(outputs)
+            correct_num += int(label == predict)
+        print(f"epoch {epoch}, test accuracy = {correct_num / len(test_data_list)}")
 
 
 if __name__ == '__main__':
